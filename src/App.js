3d0a7c0b2dcd4538bbce4b573f69683bc577de19
TYC70 <thyasin70@gmail.com>
2022-10-29 12:01:47 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        <a href='https://github.com/TYC70/agac-olusum.git' target="_blank" className='github-link'>
+          OPEN GITHUB REPOSITORY
         </a>
-      </header>
+      </div>
     </div>
   );
 }
