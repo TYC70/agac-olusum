79240087530826e23a596ffd1c646ef2192618cb
TYC70 <thyasin70@gmail.com>
2022-10-29 12:27:23 +0300

    main

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
-        <div>
-          <a href='https://github.com/TYC70/agac-olusum.git' target="_blank" className='github-link'>
-          OPEN GITHUB REPOSITORY
-          </a>
-        </div>
-        <span>Welcome to the Tree View App</span>
-        <div>
-          <button>MENU</button>
-        </div>
-      </div>
-
+      
     </div>
   );
 }
---
+++ src/MenuButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MenuButton() {
+  return (
+    <div>
+        <input placeholder="Name" type="text"/>
+        <label data-shrink="true">Self Point</label>
+    </div>
+  )
+}
